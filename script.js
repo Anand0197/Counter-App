a51eaf9044f9c7ff39cd3808f769bd6843ac8ee5
Anand <aanand.raj.patna@gmail.com>
2022-10-06 22:49:29 +0530

    Counter app

---
+++ script.js
@@ -0,0 +1,30 @@
+const count = document.querySelector('.count');
+// const sub = document.querySelector('.subtract');
+// const rest = document.querySelector('.restart');
+// const add = document.querySelector('.add');
+const btn = document.querySelector('.buttons');
+
+btn.addEventListener('click', function (e) {
+  if (e.target.classList.contains('subtract')) {
+    count.innerHTML--;
+    addcolor();
+  }
+  if (e.target.classList.contains('add')) {
+    count.innerHTML++;
+    addcolor();
+  }
+  if (e.target.classList.contains('restart')) {
+    count.innerHTML = 0;
+    addcolor();
+  }
+});
+
+function addcolor() {
+  if (count.innerHTML < 0) {
+    count.style.color = 'red';
+  } else if (count.innerHTML > 0) {
+    count.style.color = 'yellow';
+  } else {
+    count.style.color = '#0b0f0dba';
+  }
+}
